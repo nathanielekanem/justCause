169b496f8a0bae819fb707c40bd4ba3bda14d51e
Nathaniel Ekanem <evothethird@gmail.com>
2022-02-09 12:30:09 +0100

    change by admin

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,10 @@ export default function Home() {
           Welcome to JustCause!
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        // <p className={styles.description}>
+        //  Get started by editing{' '}
+        //  <code className={styles.code}>pages/index.js</code>
+        // </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
