f13e20726816ec26bcdc69fc31da0b0142fd68bc
Nathaniel Ekanem <evothethird@gmail.com>
2022-02-10 10:50:26 +0100

    cards

--- pages/index.js
+++ pages/index.js
@@ -21,30 +21,30 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="./" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>Dashboard &rarr;</h2>
+            <p>Coming soon...</p>
           </a>
 
           <a href="./" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>Music &rarr;</h2>
+            <p>Coming soon...</p>
           </a>
 
           <a
             href="./"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Books &rarr;</h2>
+            <p>Coming soon...</p>
           </a>
 
           <a
             href="./"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Movies &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Coming soon...
             </p>
           </a>
   </div>
