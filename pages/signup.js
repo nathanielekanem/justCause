662ec536fe4788fd2f23c69f22dd229a016c2bfd
Nathaniel Ekanem <evothethird@gmail.com>
2022-02-19 19:03:56 +0100

    reimplementation of signup

---
+++ pages/signup.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>~ JustCause ~ Signup</title>
+        <meta name="description" content="Join JustCause Today!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to JustCause!
+        </h1>
+      </main>
+
+      <footer className={styles.footer}>
+        {/*<a href="./">
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>*/}
+        <a href="https://www.minekglobal.com" rel="noopener noreferrer">
+          Sponsored by Minek Global
+          {/*<span className={styles.logo}>
+            <Image src="https://www.minekglobal.com/Minek%20Global%20Business%20Nigeria%20Limited_files/logo.png" alt="Minek Logo" width={72} height ={22} />
+        </span>*/}
+        </a>
+      </footer>
+    </div>
+  )
+}
