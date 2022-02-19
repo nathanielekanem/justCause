25fc107115e4ff5680daaf3b6a3516b608ea302d
Nathaniel Ekanem <evothethird@gmail.com>
2022-02-19 11:51:04 +0100

    couple of br tags added inbetween welcome note and cards

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to JustCause!
-        </h1>
+        </h1><br/><br/>
 
         <div className={styles.grid}>
           <a 
