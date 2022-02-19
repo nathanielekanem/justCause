07abac43fef417c5c0c2d7858979c470ae212a3b
Nathaniel Ekanem <evothethird@gmail.com>
2022-02-19 19:24:20 +0100

    more changes

--- pages/signup.js
+++ pages/signup.js
@@ -15,21 +15,6 @@ export default function Signup() {
           Signup
         </h1>
       </main>
-
-      <footer className={styles.footer}>
-        {/*<a href="./">
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>*/}
-        <a href="https://www.minekglobal.com" rel="noopener noreferrer">
-          Sponsored by Minek Global
-          {/*<span className={styles.logo}>
-            <Image src="https://www.minekglobal.com/Minek%20Global%20Business%20Nigeria%20Limited_files/logo.png" alt="Minek Logo" width={72} height ={22} />
-        </span>*/}
-        </a>
-      </footer>
     </div>
   )
 }
