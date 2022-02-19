a0b01d9267d6f3368096ecc78a507325d4bfbc4d
Nathaniel Ekanem <evothethird@gmail.com>
2022-02-19 18:31:13 +0100

    .

--- pages/signup.js
+++ pages/signup.js
@@ -1,10 +1,17 @@
 // justcause.vercel.app/signup
 
+import Head from 'next/head'
 import styles from '../styles/Signup.module.css'
 
 function signup() {
   return (
-      <div>
+      <div className={styles.container}>
+        <Head>
+            <title>~ JustCause ~ Signup</title>
+            <meta name="description" content="Signup to JustCause" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
         <h1 
         className={styles.header}>
             SIGNUP
