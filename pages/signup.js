7930ca6c8ce9ffdcb68e194f09729eac403375b0
Nathaniel Ekanem <evothethird@gmail.com>
2022-02-20 21:45:30 +0100

    .

--- pages/signup.js
+++ pages/signup.js
@@ -17,47 +17,47 @@ export default function Signup() {
         <div className={styles.featuresGrid}>
           <div className={styles.flyer}>
               <h2>Features</h2>
-              <ul>
+              
                   <li>Friendly General Assistant</li>
                   <li></li>
                   <li></li>
                   <li></li>
                   <li>Insights</li>
                   <li>Hardworking Details Assistant</li>
-              </ul>
+              
           </div>
           <div className={styles.flyer}>
               <h2>Entertainer</h2>
-              <ul>
+              
                   <li>Yes</li>
                   <li></li>
                   <li></li>
                   <li></li>
                   <li>Yes</li>
                   <li>Yes</li>
-              </ul>
+              
           </div>
           <div className={styles.flyer}>
               <h2>jC user</h2>
-              <ul>
+              
                   <li>Yes</li>
                   <li></li>
                   <li></li>
                   <li></li>
                   <li>No</li>
                   <li>No</li>
-              </ul>
+              
           </div>
           <div className={styles.flyer}>
               <h2>Non User</h2>
-              <ul>
+              
                   <li>No</li>
                   <li></li>
                   <li></li>
                   <li></li>
                   <li>No</li>
                   <li>No</li>
-              </ul>
+              
           </div>
         </div>
       </main>
