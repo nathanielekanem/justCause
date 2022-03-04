345e32e22194c61919faad665369e7451518af66
Nathaniel Ekanem <evothethird@gmail.com>
2022-03-04 11:12:41 +0100

    .

--- pages/signup.js/index.js
+++ pages/signup.js/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
-import styles from "../styles/Signup.module.css";
+import Head from "next/head";
+import styles from "../../styles/Signup.module.css";
 
 function Signup() {
   return (
