b3cc12f31bafd5051e6bb6a76ed980b503487ecf
Nathaniel Ekanem <evothethird@gmail.com>
2022-03-04 11:05:25 +0100

    creator

--- pages/signup.js/index.js
+++ pages/signup.js/index.js
@@ -7,14 +7,19 @@ function Signup() {
       <Head>
         <title>~ JustCause ~ Signup</title>
         <meta name="description" content="Signup on JustCause today!" />
-        <link rel="icon" href="/Dada's because nathan sucks at making logos and practically anything else!!!" />
+        <link
+          rel="icon"
+          href="/Dada's because nathan sucks at making logos and practically anything else!!!"
+        />
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>Sign Up</h1>
         <div className={styles.grid}>
           <div className={styles.card}>
-            <h2>creator &rarr;</h2>
-            <p>Signup as a content creator.</p>
+            <a href="https://justcause.vercel.app/signup/creator">
+              <h2>creator &rarr;</h2>
+              <p>Signup as a content creator.</p>
+            </a>
           </div>
           <div className={styles.card}>
             <h2>user &rarr;</h2>
