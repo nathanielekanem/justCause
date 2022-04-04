c592ddcaba5f5d9a717b50999fa7da2aa62dc60b
Nathaniel Ekanem <evothethird@gmail.com>
2022-04-04 17:26:31 +0100

    bunch of changes... check if error appears

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import "../components/js/check-login.js";
-import "../components/php/check-login.php";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
