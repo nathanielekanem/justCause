b9fee22b88e5b6633795fe60140657ed2d069363
Nathaniel Ekanem <evothethird@gmail.com>
2022-04-05 07:35:02 +0100

    ?

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import "../styles/globals.css";
 import "../components/js/check-login.js";
+import "../components/signupForm/Presignup;"
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
 export default MyApp;
