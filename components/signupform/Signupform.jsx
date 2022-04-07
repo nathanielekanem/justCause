d7b84fc883d064a1d02eb01ea5eb02f3b48e01bf
Nathaniel Ekanem <evothethird@gmail.com>
2022-04-07 07:05:45 +0100

    trial

--- components/signupform/Signupform.jsx
+++ components/signupform/Signupform.jsx
@@ -1,68 +1,93 @@
-import { Formik } from "formik";
-
-function Signupform(){
-  <Formik
-    initialValues={{
-      fullname: "",
-      username: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
-      referrerCode: "",
-    }}
-    validate={(data) => {
-      const errors = {};
-      if (!data.email) {
-        errors.email = "Required field";
-      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(data.email)) {
-        errors.email = "Invalid email address";
+/*function Signupform(){
+  const signup = async event => {
+    event.preventDefault() // stops automatic page redirection
+    
+    // Add form logic
+    const res = await fetch(
+      'https://hooks.zapier.com/hooks/catch/123456/abcde',
+      {
+        body: JSON.stringify({
+          name: event.target.name.value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
       }
-      return errors;
-    }}
-    onSubmit={(data, { setSubmitting }) => {
-      setTimeout(() => {
-        alert(JSON.stringify(data, null, 2));
-        setSubmitting(false);
-      }, 400);
-    }}
-  >
-    {({
-      data,
-      errors,
-      touched,
-      handleChange,
-      handleBlur,
-      handleSubmit,
-      isSubmitting,
-      /**And other goodies */
-    }) => (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          name="email"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={data.email}
-        />
-        {errors.email && touched.email && errors.email}
+    )
 
-        <input
-          type="password"
-          name="password"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={data.password}
-        />
-        {errors.password && touched.password && errors.password}
+    const result = await res.json()
+  }
 
-        <button type="submit" disabled={isSubmitting}>
-          Submit
-        </button>
+  return (
+    <form onSubmit={signup}>
+      <label htmlFor="fullname">Full Name</label>
+      <input id="fullname" name="fullname" type="text" autoComplete="name" required/>
+  
+      <button type="submit">Done</button>
+    </form>
+  )
+}*/
 
-        {/** Other inputs. */}
-      </form>
-    )}
-  </Formik>
+function Signupform() {
+  if (confirmPassword != password) {
+    {
+      alert("Passwords do not match!");
+    }
+  }
+
+  const callNextForm = (e) => {
+    e.preventDefault();
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="formContainer">
+      <div className="form signupForm">
+        <form onSubmit={callNextForm}>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" required />
+
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" required />
+
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" required />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" required />
+
+          <label htmlFor="confirmPassword">Confirm password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            required
+          />
+
+          <button type="submit">next</button>
+        </form>
+      </div>
+      <div className="form signupForm formHidden">
+        <form onSubmit={submit}>
+          <label htmlFor="DoB">Date of Birth</label>
+          <input type="date" name="DoB" id="DoB" required />
+
+          <label htmlFor="country">Country</label>
+          {/**Find way to get country dropdown */}
+
+          <label htmlFor="Bep20Address">Bep20 Address</label>
+          <input type="text" name="bep20Address" id="bep20address" required />
+
+          <label htmlFor="referrerCode">Referrer's Code</label>
+          <input type="text" name="refCode" id="refCode" pattern=".{,jC}" />
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
-
-export default Signupform;
